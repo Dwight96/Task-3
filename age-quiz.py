4916f48d131bb2f979ab0d600a585ebc589151e8
Dwight <dwight.edwards00@gmail.com>
2024-03-31 17:11:47 +0100

    Added comments

--- age-quiz.py
+++ age-quiz.py
@@ -1,14 +1,17 @@
-age = (input("What is your age? ")) 
+# Ask the user for their age and convert it to an integer
+age = input("What is your age? ")
 age_int = int(age)
+
+# Check various age ranges and provide corresponding messages
 if age_int >= 40 and age_int < 64:
-    print("You're over the hill.") 
-elif age_int > 100: 
-    print("Sorry you're dead.") 
+    print("You're over the hill.")  # Message for ages between 40 and 63
+elif age_int > 100:
+    print("Sorry you're dead.")  # Message for ages over 100
 elif age_int >= 65 and age_int <= 100:
-    print("Enjoy your retirement!")
-elif age_int < 13: 
-    print("You qualify for the kiddie discount.")
+    print("Enjoy your retirement!")  # Message for ages between 65 and 100
+elif age_int < 13:
+    print("You qualify for the kiddie discount.")  # Message for ages under 13
 elif age_int == 21:
-    print("Congrats on your 21st!")  
+    print("Congrats on your 21st!")  # Message for age 21
 else:
-    print("Age is but a number.")
+    print("Age is but a number.")  # Default message for all other ages
